6a6878f87ff85de79642c65455861fdfa640d2cc
cisuno7 <cisuno7@hotmail.com>
2023-09-27 16:51:03 -0300

    implementado o popup

--- src/pages/Leitura/leitura.js
+++ src/pages/Leitura/leitura.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../service/api';
 import './leituralist.css';
+
 function PontoList() {
     const [pontos, setPontos] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedPonto, setSelectedPonto] = useState({});
 
+    const [activeTab, setActiveTab] = useState('pc');
+
+console.log(selectedPonto);
     useEffect(() => {
         async function fetchPontos() {
             try {
@@ -14,31 +20,97 @@ function PontoList() {
                 console.error('Erro ao obter pontos:', error);
             }
         }
-
         fetchPontos();
     }, []);
+
+    const openModal = (ponto) => {
+        console.log(ponto)
+        setSelectedPonto(ponto);
+        setShowModal(true);
+    }
+
+    const closeModal = () => {
+        setShowModal(false);
+    }
 
     if (!pontos.length) {
         return <p>Nenhum ponto disponível.</p>;
     }
+
     return (
-        <div className="ponto-list">
-            {pontos.map(ponto => (
-                <div key={ponto.id} className="ponto-item">
-                    <div className="ponto-title">
-                        {ponto.nomeFantasia} ({ponto.nomeClinete})
-                    </div>
-                    <div className="ponto-detail">
-                        <span className="ponto-icon"></span> {ponto.endereco}, {ponto.municipio} - {ponto.estado}
-                    </div>
-                    <div className="ponto-detail">
-                        CEP: {ponto.cep}
-                    </div>
-                    <div className="ponto-detail">
-                        Ponto de Referência: {ponto.pontoDeReferencia}
-                    </div>
+        <div>
+            <div className={`modal ${showModal ? 'show' : ''}`} onClick={closeModal}>
+            <div className="modal-content" onClick={e => e.stopPropagation()}>
+    <div className="modal-scroll-container">
+    {selectedPonto && (
+    <>
+        <h1 className="modal-title">ABRIR PC LEITURA</h1>
+
+        {/* Tabs Headers */}
+        <div className="tabs">
+            <div
+                className={`tab-header ${activeTab === 'pc' ? 'active' : ''}`}
+                onClick={() => setActiveTab('pc')}
+            >
+                PC
+            </div>
+            <div
+                className={`tab-header ${activeTab === 'maqs' ? 'active' : ''}`}
+                onClick={() => setActiveTab('maqs')}
+            >
+                MAQS
+            </div>
+        </div>
+
+        {/* Tabs Contents */}
+        <div className="tab-content">
+    {activeTab === 'pc' && (
+        <div className="pc-content">
+         <div className="checkbox-container">
+         <h3>Pontos</h3>
+                <div className="checkboxes">
+                    <label>
+                        <input type="checkbox" name="checkbox1" value="1"/>
+                        Leitura
+                    </label>
+                    <label>
+                        <input type="checkbox" name="checkbox2" value="2"/>
+                       Retirada
+                    </label>
                 </div>
-            ))}
+         </div>
+            <div className="ponto-info">
+                {selectedPonto && (
+                    <>
+                        <p><strong>Nome Fantasia:</strong> {selectedPonto.nomeFantasia}</p>
+                        <p><strong>Nome Cliente:</strong> {selectedPonto.nomeClinete}</p>
+                        {/* ... outras informações do ponto ... */}
+                    </>
+                )}
+            </div>
+        </div>
+    )}
+        </div>
+    </>
+)}
+
+    </div>
+</div>
+
+            </div>
+
+            <div className="ponto-list">
+                {pontos.map(ponto => (
+                    <div key={ponto.id} className="ponto-item" onClick={() => openModal(ponto)}>
+                        <div className="ponto-title">
+                            {ponto.nomeFantasia} ({ponto.nomeClinete})
+                        </div>
+                        <div className="ponto-detail">
+                            {ponto.endereco}, {ponto.municipio} - {ponto.estado}
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
